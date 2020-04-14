223f591d12ed9eccad395d4bb24107489f67322c
Heya Kwon <heyabird@gmail.com>
2020-04-13 21:34:46 -0700

    tried fixing rspec

---
+++ spec/factories/devise.rb
@@ -0,0 +1,19 @@
+require Rails.root.join("spec/support/macros/controller_macros.rb")
+
+
+FactoryBot.define do
+    factory :user do
+      id {1}
+      email {"test@user.com"}
+      password {"qwerty"}
+      # Add additional fields as required via your User model
+    end
+  
+    # Not used in this tutorial, but left to show an example of different user types
+    # factory :admin do
+    #   id {2}
+    #   email {"test@admin.com"}
+    #   password {"qwerty"}
+    #   admin {true}
+    # end
+  end
--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -1,3 +1,4 @@
+require 'rails_helper'
 RSpec.describe History, type: :model do
   it "should validate comment" do
     history = History.create
